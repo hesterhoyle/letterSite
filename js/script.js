4c0b61a045ac67c3abf3f4800674e3a212971005
Hester Hoyle <hesterhoyle@gmail.com>
2020-03-18 11:34:56 +0000

    18th March 2020, first commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('keydown', logKey);
+
+function logKey() {
+    switch(event.code) {
+        case "KeyA":
+            document.getElementById("log").innerHTML = "A";
+            break;
+        case "KeyB":
+            document.getElementById("log").innerHTML = "B";
+            break;
+        case "KeyC":
+            document.getElementById("log").innerHTML = "C";
+            break;
+        case "KeyD":
+            document.getElementById("log").innerHTML = "D";
+            break;
+        case "KeyE":
+            document.getElementById("log").innerHTML = "E";
+            break;
+        case "KeyF":
+            document.getElementById("log").innerHTML = "F";
+            break;
+        case "KeyG":
+            document.getElementById("log").innerHTML = "G";
+            break;
+        case "KeyH":
+            document.getElementById("log").innerHTML = "H";
+            break;
+        case "KeyI":
+            document.getElementById("log").innerHTML = "I";
+            break;
+        case "KeyJ":
+            document.getElementById("log").innerHTML = "J";
+            break;
+        case "KeyK":
+            document.getElementById("log").innerHTML = "K";
+            break;
+        case "KeyL":
+            document.getElementById("log").innerHTML = "L";
+            break;
+        case "KeyM":
+            document.getElementById("log").innerHTML = "M";
+            break;
+        case "KeyN":
+            document.getElementById("log").innerHTML = "N";
+            break;
+        case "KeyO":
+            document.getElementById("log").innerHTML = "O";
+            break;
+        case "KeyP":
+            document.getElementById("log").innerHTML = "P";
+            break;
+        case "KeyQ":
+            document.getElementById("log").innerHTML = "Q";
+            break;
+        case "KeyR":
+            document.getElementById("log").innerHTML = "R";
+            break;
+        case "KeyS":
+            document.getElementById("log").innerHTML = "S";
+            break;
+        case "KeyT":
+            document.getElementById("log").innerHTML = "T";
+            break;
+        case "KeyU":
+            document.getElementById("log").innerHTML = "U";
+            break;
+        case "KeyV":
+            document.getElementById("log").innerHTML = "V";
+            break;
+        case "KeyW":
+            document.getElementById("log").innerHTML = "W";
+            break;
+        case "KeyX":
+            document.getElementById("log").innerHTML = "X";
+            break;
+        case "KeyY":
+            document.getElementById("log").innerHTML = "Y";
+            break;
+        case "KeyZ":
+            document.getElementById("log").innerHTML = "Z";
+            break;
+    }
+}
